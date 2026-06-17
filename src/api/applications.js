08fb48a6af539efd15ad48ee1cf7568b3379fab9
Ankit Bal <bingecoder89@gmail.com>
2026-06-17 11:01:13 +0530

    feat: simulate error state with a random failure

--- src/api/applications.js
+++ src/api/applications.js
@@ -1,9 +1,15 @@
 import { applications } from "../mocks/applications";
 
 export const getApplications = async () => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(applications);
+      const isFailure = Math.random() < 0.1;
+
+      if (isFailure) {
+        reject(new Error("Async promise rejected randomly!"));
+      } else {
+        resolve(applications);
+      }
     }, 1000);
   });
 };
--- src/api/graphs.js
+++ src/api/graphs.js
@@ -1,9 +1,15 @@
 import { graphs } from "../mocks/graphs";
 
 export const getGraphs = async (appId) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve(graphs[appId]);
+      const isFailure = Math.random() < 0.1;
+
+      if (isFailure) {
+        reject(new Error("Async promise rejected randomly!"));
+      } else {
+        resolve(graphs[appId]);
+      }
     }, 1000);
   });
 };
